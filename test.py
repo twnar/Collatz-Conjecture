3cb3f07f5dcb6be1b76554e93afca57cd0925078
twnar <twnarx@gmail.com>
2026-06-04 17:10:20 +0400

    test

---
+++ test.py
@@ -0,0 +1,36 @@
+for i in range(1, 1000000):
+    if i % 2 == 0:
+        i = i / 2
+    else:
+        i = 3 * i + 1
+    if i == 1:
+        print(i)
+        break
+    print(i) 
+    time.sleep(0.1)
+    if i == 1:
+        print(i)
+        break
+    print(i) 
+    time.sleep(0.1)
+    if i == 1:
+        print(i)
+        break
+    print(i) 
+    time.sleep(0.1)
+    if i == 1:
+        print(i)
+        break
+    print(i) 
+    time.sleep(0.1)
+    if i == 1:
+        print(i)
+        break
+    print(i) 
+    time.sleep(0.1)
+    if i == 1:
+        print(i)
+        break
+    print(i) 
+    time.sleep(0.1)
+                           
